dbfc7fed18b9bd774c8d1fe5482bec948d4547a7
Peter Jang <peter.x.jang@gmail.com>
2016-04-12 21:21:54 -0500

    solution for changing colors on click

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,6 +1,9 @@
 var numberOfClicks = 0;
+var colors = ["red", "green", "purple", "orange", "yellow"];
 
-function jackhammer() {
+function jackhammer(box) {
+  box.style["background-color"] = colors[numberOfClicks];
+
   numberOfClicks++;
   if (numberOfClicks === 5) {
     console.log("You win!");
